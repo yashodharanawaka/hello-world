2dcaf6f09893f98e37882d72f8fe9d0e1db8dea4
yashodharanawaka <32728666+yashodharanawaka@users.noreply.github.com>
2020-10-17 14:03:11 +0530

    annotation update

--- src/main/java/com/codespringboot/helloworld/model/Tutorial.java
+++ src/main/java/com/codespringboot/helloworld/model/Tutorial.java
@@ -9,7 +9,6 @@ import java.util.Date;
 
 
 @Document(collection = "tutorials")
-//@Component
 public class Tutorial implements Serializable {
 
     @Id
--- src/main/java/com/codespringboot/helloworld/repository/TutorialsRepository.java
+++ src/main/java/com/codespringboot/helloworld/repository/TutorialsRepository.java
@@ -2,9 +2,11 @@ package com.codespringboot.helloworld.repository;
 
 import com.codespringboot.helloworld.model.Tutorial;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface TutorialsRepository extends MongoRepository<Tutorial, String> {
 //    List<Tutorial> findByTitleIsContaining(String title);
 //    List<Tutorial> findByTitleLike(String title);
